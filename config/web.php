2356a0b680508775fd2449027c2568c9e620d77c
motae99 <motae99@gmail.com>
2017-12-26 18:00:03 +0200

    Igenor db

--- config/web.php
+++ config/web.php
@@ -149,22 +149,22 @@ $config = [
         ],
         
     ],
-    // 'as beforeRequest' => [
-    //     'class' => 'yii\filters\AccessControl',
-    //     'rules' => [
-    //         [
-    //             'allow' => true,
-    //             'actions' => ['login'],
-    //         ],
-    //         [
-    //             'allow' => true,
-    //             'roles' => ['@'],
-    //         ],
-    //     ],
-    //     'denyCallback' => function () {
-    //         return Yii::$app->response->redirect(['site/login']);
-    //     },
-    // ],
+    'as beforeRequest' => [
+        'class' => 'yii\filters\AccessControl',
+        'rules' => [
+            [
+                'allow' => true,
+                'actions' => ['login'],
+            ],
+            [
+                'allow' => true,
+                'roles' => ['@'],
+            ],
+        ],
+        'denyCallback' => function () {
+            return Yii::$app->response->redirect(['site/login']);
+        },
+    ],
     'params' => $params,
 ];
 
