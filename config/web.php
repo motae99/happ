ac26d62a8361f3cb406a5af7dcf1f750d46e1a3d
motae99 <motae99@gmail.com>
2017-12-26 18:06:24 +0200

    check production

--- config/web.php
+++ config/web.php
@@ -174,7 +174,7 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        'allowedIPs' => ['154.96.28.76', '::1'],
+        'allowedIPs' => ['154.96.28.79', '::1'],
     ];
 
     $config['bootstrap'][] = 'gii';
