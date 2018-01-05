a4c0b39a02b3ef60972648b935c6895bf15f5265
motae99 <motae99@gmail.com>
2018-01-05 05:16:17 +0200

    fixing

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=127.0.0.1;dbname=aya',
-    'username' => 'root',
-    'password' => 'motae999',
+    'username' => 'motae',
+    'password' => 'Motae_999',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
