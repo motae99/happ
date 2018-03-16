606883fab0a170e5df2cb8a449c8d273ea2e83c5
motae99 <motae99@gmail.com>
2018-03-16 20:35:04 +0200

    removing update and delete

--- views/physician/view.php
+++ views/physician/view.php
@@ -23,14 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::button('<i class="fa fa-plus"></i>', ['value' => Url::to(['physician/availability', 'id'=>$model->id]), 'title' => Yii::t('app', 'Add'), 'class' => 'btn btn-flat bg-blue showModalButton']); ?>
 
-        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
+        
     </p>
 
    
