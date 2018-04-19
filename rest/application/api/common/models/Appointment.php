5f121bbed255e753473405a0bd8caa95f7c5efed
motae99 <motae99@gmail.com>
2018-04-19 16:13:27 +0200

    comment all queue

--- rest/application/api/common/models/Appointment.php
+++ rest/application/api/common/models/Appointment.php
@@ -24,7 +24,7 @@ class Appointment extends \api\components\db\ActiveRecord
             'date' => function($model) { return $model->calender->date; },
             'from_time' => function($model) { return $model->calender->start_time; },
             'to_time' => function($model) { return $model->calender->end_time; },
-            'queue' => function($model) { return $model->queue($model); },
+            // 'queue' => function($model) { return $model->queue($model); },
             'current' => function($model) { return $model->visit($model); },
             'time' => function($model) { return $model->time($model); },
             'status',
@@ -36,16 +36,16 @@ class Appointment extends \api\components\db\ActiveRecord
         ];
     }
 
-    public function extraFields() {
-        return [
-            'schedule' => function($model) { return $model->scheduale; },
-        ];
-    }
+    // public function extraFields() {
+    //     return [
+    //         'schedule' => function($model) { return $model->scheduale; },
+    //     ];
+    // }
 
-    public function getScheduale()
-    {
-        return $this->hasMany(Schedule::className(), ['calender_id' => 'calender_id']);
-    }
+    // public function getScheduale()
+    // {
+    //     return $this->hasMany(Schedule::className(), ['calender_id' => 'calender_id']);
+    // }
 
 
     public function getCalender()
@@ -53,33 +53,33 @@ class Appointment extends \api\components\db\ActiveRecord
         return $this->hasOne(Calender::className(), ['id' => 'calender_id']);
     }
 
-    public function Queue($model)
-    {
-        $confirmed = Appointment::find()
-            ->where(['calender_id' => $model->calender_id])
-            ->andWhere(['status' => 'confirmed'])
-            // ->andWhere(['<', 'confirmed_at', $model->confirmed_at])
-            ->count();
+    // public function Queue($model)
+    // {
+    //     $confirmed = Appointment::find()
+    //         ->where(['calender_id' => $model->calender_id])
+    //         ->andWhere(['status' => 'confirmed'])
+    //         // ->andWhere(['<', 'confirmed_at', $model->confirmed_at])
+    //         ->count();
 
-        $booked = Appointment::find()
-            ->where(['calender_id' => $model->calender_id])
-            ->andWhere(['status' => 'booked'])
-            ->andWhere(['<=', 'created_at', $model->created_at])
-            ->count();
-        if ($model->status == 'booked') {
-            return $confirmed+$booked;
-        }
-        elseif($model->status == 'confirmed'){
-            $schedule = Schedule::find()
-            ->where(['appointment_id' => $model->id])
-            ->one();
-            if ($schedule) {
-                return $schedule->queue;
-            }
-        }
+    //     $booked = Appointment::find()
+    //         ->where(['calender_id' => $model->calender_id])
+    //         ->andWhere(['status' => 'booked'])
+    //         ->andWhere(['<=', 'created_at', $model->created_at])
+    //         ->count();
+    //     if ($model->status == 'booked') {
+    //         return $confirmed+$booked;
+    //     }
+    //     elseif($model->status == 'confirmed'){
+    //         $schedule = Schedule::find()
+    //         ->where(['appointment_id' => $model->id])
+    //         ->one();
+    //         if ($schedule) {
+    //             return $schedule->queue;
+    //         }
+    //     }
         
             
-    }
+    // }
 
     public function Visit($model)
     {
