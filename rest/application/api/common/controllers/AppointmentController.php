f842be6cbf9d70bef31c7501d1f7a1d45ac98a9d
motae99 <motae99@gmail.com>
2018-04-19 15:55:52 +0200

    not found error

--- rest/application/api/common/controllers/AppointmentController.php
+++ rest/application/api/common/controllers/AppointmentController.php
@@ -227,7 +227,7 @@ class AppointmentController extends \api\components\ActiveController
         }
     }
 
-    public function actionCancel($id){
+    public function actionCancel(){
                 return  array('success' => 0, 'message' => "You can't cancel this reservation");
         $user =  Yii::$app->user->identity;
         $app = Appointment::findOne($id);
